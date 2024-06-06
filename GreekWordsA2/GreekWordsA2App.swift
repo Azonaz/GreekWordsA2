424a61c4b13e411ed05c432e349da4f504e1dfb9
Azonaz <malinina.a@gmail.com>
2024-06-06 12:40:50 +0300

    Initial Commit

---
+++ GreekWordsA2/GreekWordsA2App.swift
@@ -0,0 +1,17 @@
+//
+//  GreekWordsA2App.swift
+//  GreekWordsA2
+//
+//  Created by Админ on 06/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct GreekWordsA2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
