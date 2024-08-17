d202bca1638c03f71da7779633bd005d762adcd6
Azonaz <malinina.a@gmail.com>
2024-08-17 13:50:13 +0300

    Saving the results to user defaults and checking the game progress for today

--- GreekWordsA2/Views/WordDayView.swift
+++ GreekWordsA2/Views/WordDayView.swift
@@ -12,7 +12,10 @@ struct WordDayView: View {
     @State var numberOfPoints: Int = 7
     @State private var selectedLettersStates: [Bool] = Array(repeating: false, count: 7)
     @State private var isTextVisible = false
+    @State private var isWordAlreadySolvedForToday = false
     private let radius: CGFloat = 100
+    private let userDefaults = UserDefaults.standard
+    private let solvedDateKey = "solvedDate"
 
     var body: some View {
         if isTextVisible {
@@ -31,106 +34,137 @@ struct WordDayView: View {
         GeometryReader { geometry in
             let screenWidth = geometry.size.width
             let screenHeight = geometry.size.height
-            let circleDiameter = radius * 2.7
+            let diameter = radius * 2.7
             let verticalPadding: CGFloat = 50
 
-            ZStack {
-                Circle()
-                    .fill(Color.whiteDN)
-                    .shadow(color: .grayUniversal.opacity(0.5), radius: 5, x: 2, y: 2)
-                    .frame(width: circleDiameter, height: circleDiameter)
-                    .position(x: screenWidth / 2, y: screenHeight - (circleDiameter / 2) - verticalPadding)
+            if !isWordAlreadySolvedForToday {
+                ZStack {
+                    Circle()
+                        .fill(Color.whiteDN)
+                        .shadow(color: .grayUniversal.opacity(0.5), radius: 5, x: 2, y: 2)
+                        .frame(width: diameter, height: diameter)
+                        .position(x: screenWidth / 2, y: screenHeight - (diameter / 2) - verticalPadding)
 
-                if !selectedPoints.isEmpty {
-                    Path { path in
-                        path.addLines(selectedPoints)
-                        if let currentPoint = currentPoint {
-                            path.addLine(to: currentPoint)
+                    if !selectedPoints.isEmpty {
+                        Path { path in
+                            path.addLines(selectedPoints)
+                            if let currentPoint = currentPoint {
+                                path.addLine(to: currentPoint)
+                            }
                         }
+                        .stroke(pathColor, lineWidth: 2)
                     }
-                    .stroke(pathColor, lineWidth: 2)
-                }
 
-                ForEach(0..<numberOfPoints, id: \.self) { index in
-                    let angle = 2 * .pi / Double(numberOfPoints) * Double(index)
-                    let xxx = screenWidth / 2 + radius * CGFloat(cos(angle))
-                    let yyy = screenHeight - (circleDiameter / 2) - verticalPadding + radius * CGFloat(sin(angle))
-                    let point = CGPoint(x: xxx, y: yyy)
-
-                    CharViewForGame(letter: word[index], isSelected: $selectedLettersStates[index])
-                        .position(point)
-                }
-            }
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
-            .background(Color.clear)
-            .gesture(DragGesture()
-                .onChanged { value in
-                    guard !allCirclesSelected else { return }
-                    currentPoint = value.location
-                    for index in 0..<numberOfPoints {
+                    ForEach(0..<numberOfPoints, id: \.self) { index in
                         let angle = 2 * .pi / Double(numberOfPoints) * Double(index)
-                        let xxx = screenWidth / 2 + radius * CGFloat(cos(angle))
-                        let yyy = screenHeight - (circleDiameter / 2) - verticalPadding + radius * CGFloat(sin(angle))
-                        let point = CGPoint(x: xxx, y: yyy)
-                        if distance(from: point, to: value.location) < 30 {
-                            if selectedPoints.isEmpty || selectedPoints.last != point {
-                                if !selectedPoints.contains(point) {
-                                    selectedPoints.append(point)
-                                    selectedLetters.append(word[index])
-                                    selectedLettersStates[index] = true
-                                    isTextVisible = true
+                        let x = screenWidth / 2 + radius * CGFloat(cos(angle))
+                        let y = screenHeight - (diameter / 2) - verticalPadding + radius * CGFloat(sin(angle))
+                        let point = CGPoint(x: x, y: y)
+
+                        CharViewForGame(letter: word[index], isSelected: $selectedLettersStates[index])
+                            .position(point)
+                    }
+                }
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .background(Color.clear)
+                .gesture(DragGesture()
+                    .onChanged { value in
+                        guard !allCirclesSelected && !isWordAlreadySolvedForToday else { return }
+                        currentPoint = value.location
+                        for index in 0..<numberOfPoints {
+                            let angle = 2 * .pi / Double(numberOfPoints) * Double(index)
+                            let x = screenWidth / 2 + radius * CGFloat(cos(angle))
+                            let y = screenHeight - (diameter / 2) - verticalPadding + radius * CGFloat(sin(angle))
+                            let point = CGPoint(x: x, y: y)
+                            if distance(from: point, to: value.location) < 30 {
+                                if selectedPoints.isEmpty || selectedPoints.last != point {
+                                    if !selectedPoints.contains(point) {
+                                        selectedPoints.append(point)
+                                        selectedLetters.append(word[index])
+                                        selectedLettersStates[index] = true
+                                        isTextVisible = true
+                                    }
                                 }
                             }
                         }
+                        allCirclesSelected = selectedPoints.count == numberOfPoints
                     }
-                    allCirclesSelected = selectedPoints.count == numberOfPoints
-                }
-                .onEnded { _ in
-                    if !allCirclesSelected {
-                        selectedPoints.removeAll()
-                        selectedLetters.removeAll()
-                        pathColor = .grayDN
-                        isTextVisible = false
-                        selectedLettersStates = Array(repeating: false, count: numberOfPoints)
-                    } else {
-                        let words = viewModel.grWord.split(separator: " ")
-                        let targetWord = words.last ?? words.first ?? ""
-                        if selectedLetters != targetWord {
-                            pathColor = .redUniversal
-                            DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
-                                selectedPoints.removeAll()
-                                selectedLetters.removeAll()
-                                pathColor = .grayDN
-                                allCirclesSelected = false
-                                isTextVisible = false
-                                selectedLettersStates = Array(repeating: false,
-                                                              count: numberOfPoints)
-                            }
+                    .onEnded { _ in
+                        if !allCirclesSelected {
+                            resetSelection()
                         } else {
-                            pathColor = .greenUniversal
-                            DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
-                                selectedPoints.removeAll()
-                                selectedLetters.removeAll()
-                                pathColor = .grayDN
-                                allCirclesSelected = false
-                                isTextVisible = false
-                                selectedLettersStates = Array(repeating: false,
-                                                              count: numberOfPoints)
-                            }
+                            checkSolution()
                         }
-                        currentPoint = nil
+                    }
+                )
+                .onAppear {
+                    checkIfWordSolvedToday()
+                    if !isWordAlreadySolvedForToday {
+                        updateWord(viewModel.grWord)
                     }
                 }
-            )
-            .onChange(of: viewModel.grWord) { newValue in
-                let words = newValue.split(separator: " ")
-                let targetWord = words.last ?? words.first ?? ""
-                word = Array(targetWord)
-                word.shuffle()
-                numberOfPoints = word.count
-                selectedLettersStates = Array(repeating: false, count: numberOfPoints)
+                .onChange(of: viewModel.grWord) { newValue in
+                    if !isWordAlreadySolvedForToday {
+                        updateWord(newValue)
+                    }
+                }
+            } else {
+                // Show words for today
             }
         }
+    }
+
+    private func checkSolution() {
+        let words = viewModel.grWord.split(separator: " ")
+        let targetWord = words.last ?? words.first ?? ""
+        if selectedLetters == targetWord {
+            pathColor = .greenUniversal
+            saveSolvedDate()
+        } else {
+            pathColor = .redUniversal
+        }
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.3) {
+            resetSelection()
+            pathColor = .grayDN
+        }
+    }
+
+    private func resetSelection() {
+        selectedPoints.removeAll()
+        selectedLetters.removeAll()
+        isTextVisible = false
+        selectedLettersStates = Array(repeating: false, count: numberOfPoints)
+        allCirclesSelected = false
+    }
+
+    private func updateWord(_ newValue: String) {
+        let words = newValue.split(separator: " ")
+        let targetWord = words.last ?? words.first ?? ""
+        word = Array(targetWord)
+        word.shuffle()
+        numberOfPoints = word.count
+        selectedLettersStates = Array(repeating: false, count: numberOfPoints)
+    }
+
+    private func saveSolvedDate() {
+        let today = getCurrentDate()
+        userDefaults.set(today, forKey: solvedDateKey)
+        isWordAlreadySolvedForToday = true
+    }
+
+    private func checkIfWordSolvedToday() {
+        let today = getCurrentDate()
+        if let savedDate = userDefaults.string(forKey: solvedDateKey) {
+            isWordAlreadySolvedForToday = (savedDate == today)
+        } else {
+            isWordAlreadySolvedForToday = false
+        }
+    }
+
+    private func getCurrentDate() -> String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd"
+        return formatter.string(from: Date())
     }
 
     func distance(from: CGPoint, to: CGPoint) -> CGFloat {
