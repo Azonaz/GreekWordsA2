3cf3893e5071c6a16f9f67f968f5fe7fdf266263
Azonaz <malinina.a@gmail.com>
2025-04-25 17:13:43 +0300

    Added a helper sheet with info about verb tenses

--- GreekWordsA2/Views/VerbView.swift
+++ GreekWordsA2/Views/VerbView.swift
@@ -6,6 +6,7 @@ struct VerbView: View {
     @State private var verbs: [Verb] = []
     @State private var currentIndex: Int = 0
     @State private var resetTrigger = false
+    @State private var showInfoSheet = false
     var currentVerb: Verb? {
         guard !verbs.isEmpty else { return nil }
         return verbs[currentIndex]
@@ -66,16 +67,34 @@ struct VerbView: View {
                     .foregroundColor(.white)
             }
         }
-        .navigationBarBackButtonHidden(true)
-        .navigationBarItems(leading: HStack {
-            BackButton()
-            Text("Check yourself")
-                .font(sizeClass == .regular ? .largeTitle : .title)
-                .fontWeight(.semibold)
-        })
         .onAppear {
             verbs = loadVerbs()
             verbs.shuffle()
+        }
+        .navigationBarBackButtonHidden(true)
+        .toolbar {
+            ToolbarItem(placement: .navigationBarLeading) {
+                BackButton()
+            }
+            ToolbarItem(placement: .principal) {
+                Text("Check yourself")
+                    .font(sizeClass == .regular ? .largeTitle : .title)
+                    .fontWeight(.semibold)
+                    .frame(maxWidth: .infinity, alignment: .center)
+            }
+            ToolbarItem(placement: .navigationBarTrailing) {
+                Button(action: {
+                    showInfoSheet.toggle()
+                }, label: {
+                    Image(systemName: "info.bubble")
+                        .foregroundColor(Color(.greenUniversal))
+                })
+            }
+        }
+        .sheet(isPresented: $showInfoSheet) {
+            VerbTenseView(isShowing: $showInfoSheet)
+                .presentationDetents([.height(500)])
+                .presentationDragIndicator(.visible)
         }
     }
 
@@ -86,6 +105,49 @@ struct VerbView: View {
             return []
         }
         return decoded
+    }
+}
+
+struct VerbTenseView: View {
+    @Binding var isShowing: Bool
+
+    var body: some View {
+        ZStack {
+            Color.grayDN
+                .edgesIgnoringSafeArea(.all)
+
+            ScrollView {
+                Text("Verb tenses")
+                    .font(.title3)
+                    .foregroundColor(.greenUniversal)
+                    .padding(.top)
+                    .padding(.bottom, 10)
+
+                VStack(alignment: .leading, spacing: 10) {
+                    // swiftlint:disable line_length
+
+                    Text("Ενεστώτας ")
+                        .fontWeight(.bold)
+                        .foregroundColor(.greenUniversal) +
+                    Text("can express things that are happening now, things that happen regularly, or general truths. It's one of the basic tenses used to talk about what is true or happening right now.")
+
+                    Text("Στιγμιαίος Μέλλοντας ")
+                        .fontWeight(.bold)
+                        .foregroundColor(.greenUniversal) +
+                    Text("is used to express a single completed action that will happen in the future. It is similar to the Simple Future Tense in English.")
+
+                    Text("Αόριστος ")
+                        .fontWeight(.bold)
+                        .foregroundColor(.greenUniversal) +
+                    Text("is a past tense used to describe actions that were completed in the past, without indicating when exactly the action occurred or how long it lasted. This time it captures an event that has happened only once, an action that ended in the past. It's similar to the Simple Past Tense in English.")
+                    // swiftlint:enable line_length
+                }
+                .foregroundColor(.blackDN)
+                .padding(.horizontal)
+            }
+            .frame(maxHeight: .infinity)
+            .padding(.horizontal)
+        }
     }
 }
 
